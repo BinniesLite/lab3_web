c7f5090538ebaa95d941ed43731b7bbe7deed792
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2023-02-13 22:02:23 -0500

    Add a lot of thing

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// Generate a functional component called Contact
+const Contact = () => {
+
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// Write a functional component called Navbar
+
+const Navbar = () => {
+    
+    return (
+        // create material-ui navbar
+        // https://material-ui.com/components/app-bar/
+        <div>Navbar</div>
+    )
+}
