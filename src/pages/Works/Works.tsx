c74bffc38a30bce2b601259b4fda85e95ad9f3e1
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2023-03-28 13:37:04 -0400

    Using React Query

--- src/pages/Works/Works.tsx
+++ src/pages/Works/Works.tsx
@@ -1,27 +1,19 @@
 import React from 'react'
 
 import { Stack, Divider, Box } from '@mui/material';
-import { makeStyles } from '@material-ui/core';
 
 import Section from '../../components/Section';
 import WorkCard from '../../components/Works/WorkCard';
-
 import workHeader from '../../assets/work.jpg';
 
 import { getAllProjects } from '../../api/http-request';
 import Typography from '@mui/material/Typography';
-
 import { ScrollToTop } from '../../utils/scroll-top';
 
-import Wrapper from '../../layout/Wrapper';
 import Image from '../../components/Image';
 import { Projects } from '../types/types';
-const useStyles = makeStyles({
-  root: {
-    border: 'none',
-    boxShadow: 'none',
-  },
-});
+
+import { useQuery } from '@tanstack/react-query';
 
 
 // write interface for projects type
@@ -30,20 +22,33 @@ const useStyles = makeStyles({
 
 const Works = () => {
   const [projects, setProjects] = React.useState<Projects[]>([]);
-
-  React.useEffect(() => {
-    getAllProjects().then((res) => {
-      setProjects(res.data);
+  const {data, isLoading, error} = useQuery(['projects-main'], () => {
+    return getAllProjects().then((res) => {
+      return res.data
     })
-  }, [])
+  }, 
+  {
+    cacheTime: Infinity
+  }
+  )
+  
 
+
+  if (error) {
+    console.log(error);
+  }
+  
+
+
+
+  
 
   return (
     <Stack sx={{ width: '100%', backgroundColor: '' }}>
       <ScrollToTop />
       <Section>
-        <Stack flexDirection={{ md: 'row', xs: 'column' }} justifyContent="space-between">
-          <Typography variant="h1" fontWeight="500">
+        <Stack flexDirection={{ md: 'row', xs: 'column' }} rowGap={3} justifyContent="space-between">
+          <Typography variant='h2' fontWeight="500">
             Projects
           </Typography>
           <Typography gutterBottom variant='h5' color="text.primary">
@@ -52,16 +57,18 @@ const Works = () => {
 
         </Stack>
         <Box 
-          sx={{ my: 4 }}>
+          sx={{ my: 4, display: {xs: 'none', md: 'block'} }}>
             <Image style={{ maxWidth: '100%', height: 'auto' }} src={workHeader} alt="" />
           </Box>
         <Divider sx={{ borderBottomWidth: '2px', fontWeight: 'bold', height: 5, borderColor: 'black', my: 10 }} />
         <Stack columnGap={5} flexWrap="wrap" flexDirection="row">
-          <>
-            {projects.map((project, key) => (
+          {!isLoading &&
+            <>
+            {data.map((project: any, key: any) => (
               <WorkCard id={project.id} key={key} img={project.image} date={project.date} description={project.description} title={project.name} />
             ))}
           </>
+          }
         </Stack>
 
       </Section>
