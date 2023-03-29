fe62b6688fcc2b07a380b483bb86e1d6cf9ea1dc
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2023-03-29 00:37:37 -0400

    Add all

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, {lazy, Suspense} from 'react';
 import Navbar from './components/Navbar/Navbar'; 
 const Footer = lazy(() => import('./components/Footer/Footer'));
 import Home from './pages/Home';
-import Contact from './pages/Contact/Contact';
-import Team from './pages/Team/Team';
-import Works from './pages/Works/Works';
-import WorkDetail from './pages/Works/WorkDetail';
-import Events from './pages/Events/Events';
-import EventDetail from './pages/Events/EventDetail';
+// lazy load
+const Events = lazy(() => import('./pages/Events/Events'));
+const EventDetail = lazy(() => import('./pages/Events/EventDetail'));
+const Works = lazy(() => import('./pages/Works/Works'));
+const WorkDetail = lazy(() => import('./pages/Works/WorkDetail'));
+const Team = lazy(() => import('./pages/Team/Team'));
+const Contact = lazy(() => import('./pages/Contact/Contact'));
+
 
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -25,14 +27,17 @@ function App() {
         <Router>
         <Navbar/>
           <Routes>
+            {/* use suspsense for this  */}
             <Route path="/" element={<Home/>} />
             <Route path="/contact" element={<Contact/>}/>
-            <Route path="/team" element={<Team/>}/>
+            <Route path="/team" 
+              element={<Suspense fallback={<></>}> <Team/></Suspense>}
+            />
       
-            <Route path="/work" element={<Works/>}/>
-            <Route path="/work/:id" element={<WorkDetail/>}/>
-            <Route path="/events" element={<Events/>}/>
-            <Route path="/events/:id" element={<EventDetail/>}/>
+            <Route path="/work" element={<Suspense fallback={<></>}> <Works/></Suspense>}/>
+            <Route path="/work/:id" element={<Suspense fallback={<></>}> <WorkDetail/></Suspense>}/>
+            <Route path="/events" element={<Suspense fallback={<></>}> <Events/></Suspense>}/>
+            <Route path="/events/:id" element={<Suspense fallback={<></>}> <EventDetail/></Suspense>}/>
           </Routes>
         </Router>
         <Suspense fallback="">
